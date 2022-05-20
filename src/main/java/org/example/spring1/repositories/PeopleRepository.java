a0cd4f47260621b31b37225fac1c67d638fcc3c5
pasha27 <pachademon.shevelev@gmail.com>
2022-05-20 19:41:05 +0300

    SpringDataJPACRUDApp

---
+++ src/main/java/org/example/spring1/repositories/PeopleRepository.java
@@ -0,0 +1,9 @@
+package org.example.spring1.repositories;
+
+import org.example.spring1.models.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PeopleRepository extends JpaRepository<Person, Integer> {
+}
---
+++ src/main/java/org/example/spring1/services/PeopleService.java
@@ -0,0 +1,47 @@
+package org.example.spring1.services;
+
+import org.example.spring1.models.Person;
+import org.example.spring1.repositories.PeopleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class PeopleService {
+
+    private final PeopleRepository peopleRepository;
+
+    @Autowired
+    public PeopleService(PeopleRepository peopleRepository) {
+        this.peopleRepository = peopleRepository;
+    }
+
+    public List<Person> findAll() {
+        return peopleRepository.findAll();
+    }
+
+    public Person findOne(int id) {
+        Optional<Person> foundPerson = peopleRepository.findById(id);
+        return foundPerson.orElse(null);
+    }
+
+    @Transactional
+    public void save(Person person) {
+        peopleRepository.save(person);
+    }
+
+    @Transactional
+    public void update(int id, Person updatedPerson) {
+        updatedPerson.setId(id);
+        peopleRepository.save(updatedPerson);
+    }
+
+    @Transactional
+    public void delete(int id) {
+        peopleRepository.deleteById(id);
+    }
+}
